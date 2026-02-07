3b743a1193d49343d50eb0b11b6bfe1a41af2771
Mirkamol <mirkamol.m.a@gmail.com>
2026-02-07 15:29:48 +0500

    feat(OrderServiceTest): added tests for Mockito argument matchers

--- src/test/java/com/mirkamolcode/OrderServiceTest.java
+++ src/test/java/com/mirkamolcode/OrderServiceTest.java
@@ -9,6 +9,7 @@ import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.math.BigDecimal;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
@@ -32,5 +33,30 @@ class OrderServiceTest {
         // then
         Mockito.verify(stripePaymentProcessor).charge(BigDecimal.TEN);
         assertThat(actual).isTrue();
+    }
+
+    @Test
+    void testAnyMatcher() {
+        // given
+        Map<String, String> mockMap = mock();
+        // when
+        when(mockMap.get(anyString())).thenReturn("Hello");
+        // then
+        assertThat(mockMap.get("1")).isEqualTo("Hello");
+        assertThat(mockMap.get("2")).isEqualTo("Hello");
+        verify(mockMap, times(2)).get(anyString());
+    }
+
+    @Test
+    void testEqMatcher() {
+        // given
+        Map<String, String> mockMap = mock();
+        // when
+        when(mockMap.put(anyString(), eq("1"))).thenReturn("hello");
+        // then
+        String actual = mockMap.put("hello", "1");
+        assertThat(actual).isEqualTo("hello");
+        verify(mockMap).put(eq("hello"), eq("1"));
+
     }
 }
